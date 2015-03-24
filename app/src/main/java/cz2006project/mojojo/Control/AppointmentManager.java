187aa3de0717a066eb84d92945a951ff91521d78
DoomCarnage <Nexus9th@gmail.com>
2015-03-25 01:24:52 +0800

    Edited AppointManager

--- app/src/main/java/cz2006project/mojojo/Control/AppointmentManager.java
+++ app/src/main/java/cz2006project/mojojo/Control/AppointmentManager.java
@@ -3,13 +3,19 @@ package main.java.cz2006project.mojojo.Control;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import main.java.cz2006project.mojojo.Entity.Appointment;
+
 /**
  * Created by Dhruv again on 2/24/2015.
  */
 
-public class AppointmentManager {
+    public class AppointmentManager {
 
     private ArrayList<Appointment> AppointmentList;
+    private ParseQuery<Appointment> Query = null;
     public void AppointmentManager(){
 
 
@@ -30,10 +36,25 @@ public class AppointmentManager {
 
     }
 
-    public Boolean VerifyFollowUpAppointment(ParseObject appointment , String ){
-
-        ParseQuery<ParseObject> query = ParseQuery.getQuery(appointment.getObjectId());
-        return true;
+    public Boolean VerifyFollowUpAppointment(int AppointmentNo)
+    {
+        Query = new ParseQuery<Appointment>("Appointment");
+        Query = ParseQuery.getQuery("Appointment");
+        Query.whereEqualTo("AppointmentNo",AppointmentNo);
+        try
+        {
+            AppointmentList = (ArrayList)Query.find();
+            for(Appointment Appt : AppointmentList)
+            {
+                if(Appt.IsFollowUpAppointment())
+                    return true;
+            }
+        }
+        catch (com.parse.ParseException e)
+        {
+                e.printStackTrace();
+        }
+        return false;
     }
 
     public Boolean ValidateAppointment(){
