0ae029980155f94664fe611588935e665a3d22d5
icecoolcat <LEES0152@e.ntu.edu.sg>
2015-03-24 15:07:46 +0800

    Doctor Manager Updated

--- app/src/main/java/cz2006project/mojojo/Control/DoctorManager.java
+++ app/src/main/java/cz2006project/mojojo/Control/DoctorManager.java
@@ -4,4 +4,19 @@ package main.java.cz2006project.mojojo.Control;
  * Created by ngshuling on 23/3/15.
  */
 public class DoctorManager {
+    pubic void AuthenticateDoctor(){
+
+    }
+
+    public void PrescribeMedicine() {
+
+    }
+
+    public void UpdateSchedule() {
+
+    }
+
+    public void ViewMedicalRecord() {
+
+    }
 }
