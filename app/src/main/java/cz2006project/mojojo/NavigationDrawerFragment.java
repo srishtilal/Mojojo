3a0daf4e43c250a6426ea979c5171f825fc83082
bluepearlling <maisurahshuling@gmail.com>
2015-04-01 12:51:08 +0800

    to be reverted back

--- app/src/main/java/cz2006project/mojojo/NavigationDrawerFragment.java
+++ app/src/main/java/cz2006project/mojojo/NavigationDrawerFragment.java
@@ -143,7 +143,7 @@ public class NavigationDrawerFragment extends Fragment {
 
         //mDrawerListView.addFooterView(mDrawerLinearLayout);
 
-        mProfilePic = ((ParseCircularImageView) mDrawerLinearLayout.findViewById(R.id.nav_drawer_profile_pic));
+        /*mProfilePic = ((ParseCircularImageView) mDrawerLinearLayout.findViewById(R.id.nav_drawer_profile_pic));*/
         mProfilePic.setPlaceholder(getResources().getDrawable(R.drawable.com_facebook_profile_default_icon));
 
 
