72e3145c2dbdcc9abb10d02b46c1dec28008d008
srishtilal <srishti_lal@hotmail.com>
2015-04-01 17:15:10 +0800

    Merge branch 'master' of https://github.com/srishtilal/Mojojo

--- app/src/main/java/cz2006project/mojojo/Boundary/DoctorActivity.java
+++ app/src/main/java/cz2006project/mojojo/Boundary/DoctorActivity.java
@@ -141,7 +141,7 @@ public class DoctorActivity extends ActionBarActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragmentDoctor.isDrawerOpen()) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
             // Only show items in the action bar relevant to this screen
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
